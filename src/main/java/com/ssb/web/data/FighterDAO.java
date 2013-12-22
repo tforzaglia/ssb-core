62b51933315461831445f830d97b3f962dca3a74
Tom Forzaglia <thomas.forzaglia@gmail.com>
2013-12-21 22:58:57 -0500

    doing year string parsing more intelligently now

--- src/main/java/com/ssb/web/data/FighterDAO.java
+++ src/main/java/com/ssb/web/data/FighterDAO.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.ssb.web.model.Fighter;
-import com.ssb.web.utils.DatabaseUtils;
 
 public class FighterDAO {
 	
@@ -35,7 +34,7 @@ public class FighterDAO {
 	// retrieve the number of wins for the specified fighter and year
 	public Integer getWinsByYear(int year, String name) {		
 		
-		String yearString = DatabaseUtils.getYearColumn(year);
+		String yearString = "YEAR" + year + "_WINS";
 		String sql = "SELECT " + yearString + " FROM FIGHTERS WHERE NAME = ?";
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource); 
 		Integer wins = (Integer) jdbcTemplate.queryForObject(sql, new Object[] { name }, Integer.class);
@@ -46,7 +45,7 @@ public class FighterDAO {
 	// update the wins column for the specified fighter and year
 	public void updateWinsByYear(Fighter fighter, int year) {
 		
-		String yearString = DatabaseUtils.getYearColumn(year);
+		String yearString = "YEAR" + year + "_WINS";
 		String sql = "UPDATE FIGHTERS SET " + yearString + " = ? WHERE NAME = ?";
 		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource); 
 		jdbcTemplate.update(sql, new Object[] { fighter.getWinsThroughTheYears().get(year - 1), fighter.getName() });
